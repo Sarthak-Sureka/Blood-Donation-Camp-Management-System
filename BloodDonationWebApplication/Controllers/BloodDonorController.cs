56d17eb25a6ba4bc43e6c71a09f403dceb25ff28
Sarthak Sureka <sarthaksureka97@gmail.com>
2019-09-12 10:38:39 +0530

    Commit

---
+++ BloodDonationWebApplication/Controllers/BloodDonorController.cs
@@ -0,0 +1,30 @@
+﻿using BloodDonation.DataAccess;
+using BloodDonation.Entities;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BloodDonationWebApplication.Controllers
+{
+    public class BloodDonorController : ApiController
+    {
+        private BloodDonorRepository bdr = new BloodDonorRepository();
+
+        [Route("api/getdetails")]
+        public List<DonorDetails> GetDetails()
+        {
+             return JsonConvert.DeserializeObject<List<DonorDetails>>(bdr.ReadDonorDetails());
+        }
+
+        [Route("api/postdetails/{jsondata}")]
+        public void PostDetails(string jsondata)
+        {
+            bdr.SaveDonorDetails(jsondata);
+        }
+
+    }
+}
