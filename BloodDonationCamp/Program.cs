11077801063406542a832668cca9788184229670
Sarthak Sureka <sarthaksureka97@gmail.com>
2019-09-13 12:30:18 +0530

    Commit

--- BloodDonationCamp/Program.cs
+++ BloodDonationCamp/Program.cs
@@ -21,14 +21,14 @@ namespace BloodDonationCamp
             //    BloodGroup = "A+",
             //    UnitCollected = 500
             //};
-            IBloodDonorRepository bd = new BloodDonorRepository();
-            string data = @"{'DonorID': 7, 'Name':'Sarthak Sureka','DOB':'06/08/1997','Weight': 70,'BloodGroup':'B+','UnitCollected': 750}";
+            //IBloodDonorRepository bd = new BloodDonorRepository();
+            //string data = @"{'DonorID': 7, 'Name':'Sarthak Sureka','DOB':'06/08/1997','Weight': 70,'BloodGroup':'B+','UnitCollected': 750}";
             //bd.SaveDonorDetails(data);
             //Console.WriteLine(bd.ReadDonorDetails());
             //bd.DeleteDonorDetails(1);
             //Console.WriteLine();
             //Console.WriteLine(bd.ReadDonorDetails());
-            bd.UpdateDonorDetails(data);
+            //bd.UpdateDonorDetails(data);
             //Console.WriteLine();
             //Console.WriteLine(bd.ReadDonorDetails());
         }
